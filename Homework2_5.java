7360d6cdb0c929ffb47de7b7238adbbfd67be7a3
JuliyaB <50882888+JuliyaB@users.noreply.github.com>
2019-07-31 13:44:54 +0300

    Add files via upload

---
+++ Homework2_5.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class Homework2_5 extends Thread {
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE / 2;
+
+    public static void main(String[] args) {
+        Homework2_5 myThread = new Homework2_5();
+        myThread.start();
+    }
+
+    @Override
+    public void run() {
+        method1();
+        method2();
+    }
+
+    public synchronized void method1() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.printf("Время работы method1: %d", System.currentTimeMillis() - a);
+        System.out.println();
+    }
+
+    public synchronized void method2() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        float[] a1 = new float[HALF];
+        float[] a2 = new float[HALF];
+        System.arraycopy(arr, 0, a1, 0, HALF);
+        System.arraycopy(arr, HALF, a1, 0, HALF);
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a1.length; i++)
+                    a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        };
+        Thread t2 = new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < a2.length; i++)
+                    a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        };
+
+        t.start();
+        t2.start();
+        try {
+            t.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(a1, 0, arr, 0, HALF);
+        System.arraycopy(a2, 0, arr, HALF, HALF);
+
+        System.out.printf("Время работы method2: %d", System.currentTimeMillis() - a);
+        System.out.println();
+    }
+}
