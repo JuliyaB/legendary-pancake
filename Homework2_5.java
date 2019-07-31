3b32e3741257f60ba1e18ba6e0032e0205790125
JuliyaB <50882888+JuliyaB@users.noreply.github.com>
2019-07-31 16:16:56 +0300

    Update Homework2_5.java

--- Homework2_5.java
+++ Homework2_5.java
@@ -38,33 +38,30 @@ public class Homework2_5 extends Thread {
         float[] a2 = new float[HALF];
         System.arraycopy(arr, 0, a1, 0, HALF);
         System.arraycopy(arr, HALF, a1, 0, HALF);
-        Thread t = new Thread() {
+        Thread thread = new Thread() {
             @Override
             public void run() {
                 for (int i = 0; i < a1.length; i++)
                     a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         };
-        Thread t2 = new Thread(){
+        Thread thread2 = new Thread(){
             @Override
             public void run() {
                 for (int i = 0; i < a2.length; i++)
                     a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         };
-
-        t.start();
-        t2.start();
+        thread.start();
+        thread2.start();
         try {
-            t.join();
-            t2.join();
+            thread.join();
+            thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         System.arraycopy(a1, 0, arr, 0, HALF);
         System.arraycopy(a2, 0, arr, HALF, HALF);
-
         System.out.printf("Время работы method2: %d", System.currentTimeMillis() - a);
         System.out.println();
     }
